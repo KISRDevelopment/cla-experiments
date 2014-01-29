bbee5a2d5e786fddd1df09885c8db4e55079f195
Octowl <alsaff1987@gmail.com>
2014-01-29 11:41:26 +0300

    Changed import statements in PDFScraper.py. Tested layout analyser.

--- MoJ/PDFScraper/PDFScraper.py
+++ MoJ/PDFScraper/PDFScraper.py
@@ -1,6 +1,18 @@
 """NONE OF THIS CRAP WORKS YET!!!"""
 
+import sys
+from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfparser import PDFParser
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.pdfdevice import PDFDevice, TagExtractor
+from pdfminer.pdfpage import PDFPage
+from pdfminer.converter import XMLConverter, HTMLConverter, TextConverter
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.cmapdb import CMapDB
+from pdfminer.layout import LAParams
+from pdfminer.image import ImageWriter
+
+"""from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
 from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfpage import PDFTextExtractionNotAllowed
@@ -8,7 +20,7 @@ from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.pdfinterp import PDFPageInterpreter
 from pdfminer.pdfdevice import PDFDevice
 from pdfminer.layout import LAParams
-import sys
+import sys"""
 
 # Open a PDF file.
 fp = open(sys.argv[1], 'rb')
@@ -18,14 +30,17 @@ parser = PDFParser(fp)
 document = PDFDocument(parser)
 # Supply the password for initialization.
 # (If no password is set, give an empty string.)
-document.initialize(password)
+document.initialize('')
 # Check if the document allows text extraction. If not, abort.
 if not document.is_extractable:
     raise PDFTextExtractionNotAllowed
 # Create a PDF resource manager object that stores shared resources.
 rsrcmgr = PDFResourceManager()
+# Set parameters for analysis.
+laparams = LAParams()
 # Create a PDF device object.
-device = PDFDevice(rsrcmgr)
+######device = PDFDevice(rsrcmgr)
+device = PDFPageAggregator(rsrcmgr, laparams=laparams)
 # Create a PDF interpreter object.
 interpreter = PDFPageInterpreter(rsrcmgr, device)
 # Process each page contained in the document.
@@ -33,3 +48,6 @@ for page in PDFPage.create_pages(document):
     interpreter.process_page(page)
     # recieve the LTPage object for the page.
     layout = device.get_result()
+    ### PRINT FOR TESTING ###
+    for obj in layout:
+        print obj
